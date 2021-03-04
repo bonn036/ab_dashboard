013b8ced726fc637d52c17744d8d6f706f009bd9
james036 <bonn036@gmail.com>
2021-03-04 18:47:18 +0800

    update

--- docs/static/js/chunk-2d0abfcc.82cd7c9f.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0abfcc"],{"182a":function(t,e,n){"use strict";n.r(e);var c=function(t,e){var n=e._c;return n("div",{staticStyle:{padding:"30px"}},[n("el-alert",{attrs:{closable:!1,title:"menu 1-3",type:"success"}})],1)},a=[],l=n("2877"),s={},u=Object(l["a"])(s,c,a,!0,null,null,null);e["default"]=u.exports}}]);
-//# sourceMappingURL=chunk-2d0abfcc.82cd7c9f.js.map
